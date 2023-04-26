6a828ca88719a81a86313b99d2011215ba3955ad
ZurrBot <131641406+ZurrBot@users.noreply.github.com>
2023-04-26 17:14:43 +0700

    Update backup-script.js

--- src/commands/owner/backup-script.js
+++ src/commands/owner/backup-script.js
@@ -11,9 +11,9 @@ export default {
         let dir = fs.readdirSync(".")
         let file = dir.filter(a => a !== "node_modules" && a !== "package-lock.json" && a !== ".git" && a !== global.session.Path)
         mess("wait", m)
-        let exec = await execSync(`zip -r hisoka.zip ${file.join(" ")}`)
+        let exec = await execSync(`zip -r ZurrBot.zip ${file.join(" ")}`)
         await hisoka.sendMessage(global.options.owner[0] + "@s.whatsapp.net", "./hisoka.zip", { quoted: m, fileName: "hisoka-wawebjs-backup.zip" })
-        fs.unlinkSync("./hisoka.zip")
+        fs.unlinkSync("./Zurr.zip")
     },
     isOwner: true
 }
